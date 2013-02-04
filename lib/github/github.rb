c4422ceef7a4925fe9ceeb8ca0d1651cb7909ead
Ramsey Nasser <ram@nas.sr>
2013-02-04 18:32:55 -0500

    Clean up long line of code in github.rb

--- lib/github/github.rb
+++ lib/github/github.rb
@@ -6,7 +6,8 @@ module Gozer
   class Stream
     class Github < Stream
       def self.new user, project, branch="master"
-        JSON.parse(open("https://api.github.com/repos/#{user}/#{project}/commits").read).map {|c|
+        api_url = "https://api.github.com/repos/#{user}/#{project}/commits"
+        JSON.parse(open(api_url).read).map {|c|
           c = Hashie::Mash.new(c)
           i = Item.new Time.parse(c.commit.author.date)
 
