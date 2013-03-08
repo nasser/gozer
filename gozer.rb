fc4493d89add33b2c0e276c941d806a00e5c1a22
Ramsey Nasser <ram@nas.sr>
2013-03-08 18:23:04 -0500

    Add index page, json and html demo pages

--- gozer.rb
+++ gozer.rb
@@ -9,15 +9,36 @@ require_relative "lib/gozer/item"
 
 require_relative "lib/atom/atom"
 require_relative "lib/github/github"
+require_relative "lib/tumblr/tumblr"
+
+def demo_stream
+  stream = Gozer::Stream.new []
+  # stream += Gozer::Stream::Github.new "openFrameworks", "openFrameworks"
+  # stream += Gozer::Stream::Github.new "nasser", "zajal", "amsterdam"
+  # stream += Gozer::Stream::Atom.new "http://blog.nas.sr/rss"
+  Gozer::Stream::Tumblr.api_key = "zZjfj7R30K17nG35Nqw6OXAN3QMxHp31veEaf1frJ7xzIAqj3p"
+  stream += Gozer::Stream::Tumblr.new "wearableweapons.tumblr.com"  
+
+  stream
+end
 
 get "/" do
+  erb :index
+end
+
+get "/demo.json" do
   content_type 'application/json'
+  demo_stream.to_json
+end
 
-  stream = Gozer::Stream.new []
-  stream += Gozer::Stream::Github.new "openFrameworks", "openFrameworks"
-  stream += Gozer::Stream::Github.new "nasser", "zajal", "amsterdam"
-  stream += Gozer::Stream::Atom.new "http://blog.nas.sr/rss"
-  stream += Gozer::Stream::Atom.new "http://blog.zajal.cc/rss"
+get "/demo.html" do
+  content_type 'text/html'
 
-  stream.to_json
+  demo_stream.map do |item|
+    "<div class='stream-item' id='item-#{item.object_id}'>" +
+    item.keys.map do |key| 
+      "<span class='#{key}'>#{item[key]}</span>"
+    end.join("\n") +
+    "</div>"
+  end.join("\n")
 end
