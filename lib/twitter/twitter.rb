a8816060cbaad310947032e476c59a71d85e5438
Ramsey Nasser <ram@nas.sr>
2013-05-08 14:41:43 -0400

    Add twitter stream
    Basic filtering in place, no differentiation of media yet

---
+++ lib/twitter/twitter.rb
@@ -0,0 +1,32 @@
+require "twitter"
+require "json"
+require "open-uri"
+require "hashie"
+
+require_relative "../gozer/stream"
+require_relative "../gozer/item"
+
+module Gozer
+  class Stream
+    class Twitter < Stream
+      class << self
+        attr_accessor :credentials
+      end
+
+      def self.new handle, tags=[]
+        twitter = ::Twitter::Client.new Twitter.credentials
+        twitter.user_timeline(handle, count:100).map do |t|
+          next if not tags.empty? and not t.hashtags.any? { |h| tags.include? h.text }
+
+          i = Item.new t.created_at
+
+          i.content = t.text
+          i.author = t.user.name
+          i.image = t.user.profile_image_url
+
+          i
+        end.compact
+      end
+    end
+  end
+end
