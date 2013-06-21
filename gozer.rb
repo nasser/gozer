25904366ea317baa286dace9aefaa355db585428
Ramsey Nasser <ram@nas.sr>
2013-06-21 17:43:13 -0400

    Fix quotes bug

--- gozer.rb
+++ gozer.rb
@@ -59,7 +59,6 @@ def demo_stream options={}
   options["cache_time"] ||= 3600
   options["page"] ||= nil
   options["items_per_page"] ||= 25
-
   p options
 
   @@last_update ||= nil
@@ -78,7 +77,7 @@ def demo_stream options={}
   else
     @@cached_stream
 
-  end.to_json
+  end
 end
 
 get "/demo.json" do
